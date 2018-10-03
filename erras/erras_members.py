dfa5b6e5cf67166deb74cd35aa246b9c5d0a9df2
Steven J. Owens <stevenjowens@gmail.com>
2018-10-03 14:08:17 -0400

    Added timestamp method; added uniform sorting of custom field names; changed from try/except to with open; moved code at end into a main function

--- erras/erras_members.py
+++ erras/erras_members.py
@@ -37,6 +37,10 @@ class ErrasFiles(object):
         self.keypad_field_names = keypad_field_names
         self.rfid_field_names = rfid_field_names
         
+    def get_timestamp(self):
+        format_string = "%Y%m%d-%H%M%S"
+        return datetime.datetime.utcnow().strftime(format_string)
+
     # For debugging the json returned by wildapricot, not currently used.
     def log_contact(self, contact):
         default_field_name = "default"
@@ -79,23 +83,20 @@ class ErrasFiles(object):
                    "Email",
                    "Status",
                    "Url"]
-        for name in sorted(self.keypad_field_names):
-            headers.append(name)
-        for name in sorted(self.rfid_field_names):
+        for name in sorted(self.keypad_field_names + self.rfid_field_names):
             headers.append(name)
         return headers
     
-    def strip(self, foo):
-        if foo is not None:
-            foo2 = foo.strip()  # Remove any leading/trailing whitespace
-            foo2 = foo2.lstrip("0") # Remove any leading zeroes to avoid python interpreting it as octal
-            return foo2
+    def strip(self, some_string):
+        if some_string is not None:
+            stripped_string = some_string.strip()  # Remove any leading/trailing whitespace
+            stripped_string = stripped_string.lstrip("0") # Remove any leading zeroes to avoid python interpreting it as octal
+            return stripped_string
 
     def contact_to_list(self, contact):
         field_values = dict()
-        for name in self.keypad_field_names:
-            field_values[name] = self.strip(self.get_field_value(contact, name, "default"))
-        for name in self.rfid_field_names:
+        # combined list doesn't need to be sorted because it's going into a dict
+        for name in self.keypad_field_names + self.rfid_field_names:   # TODO:  Should this be a set union?
             field_values[name] = self.strip(self.get_field_value(contact, name, "default"))
 
         contact_list = [contact.Id,
@@ -105,8 +106,11 @@ class ErrasFiles(object):
                 contact.Email,
                 contact.Status,
                 contact.Url]
-        for name, value in field_values.items():
-            contact_list.append(value)
+#        for name in sorted(field_values):
+#            print("For %s sorted key name is %s, value is %s" % (contact.DisplayName, name, field_values[name]))
+
+            contact_list.append(field_values[name])
+#        print("contact_to_list, returning: %s" % ", ".join(map(str, contact_list)))
         return contact_list
     
     def get_field_value(self, contact, fieldname, defaultvalue=""):
@@ -123,13 +127,10 @@ class ErrasFiles(object):
     def print_contacts_csv(self, contacts, filename):
         contacts_list = self.contacts_to_list(contacts)
         self.log.debug("Writing data to filename %s" % filename)
-        filehandle = open(filename, 'wt')
-        try:
+        with open(filename, 'wt') as filehandle:
             writer = csv.writer(filehandle, quoting=csv.QUOTE_NONNUMERIC, dialect='excel')
             for row in contacts_list:
                 writer.writerow(row)
-        finally:
-            filehandle.close()
     
     # Not used in current version, but very handy for debugging, so keeping.
     def todict(self, obj, classkey=None):
@@ -203,112 +204,116 @@ class ErrasFiles(object):
 ######################################################################
 # Set up configuration variables.
 
-# TODO:  delete this default dict after testing fallback.
-# default_confs = {
-#    "filter_query" : "status eq Active or status eq 'Pending - Renewal'",
-#    "csv_backup_filename_root" : "erras_backup_members_",
-#    "csv_filename_temp" : "erras_members_new.csv",
-#    "csv_filename" : "erras_members.csv",
-#    "apricot_response_root" : "wild_apricot_response_",
-#    "members_log_filename" : "erras_members.log",
-#    "keypad_field_names" : "Keypad",
-#    "rfid_field_names" : "RFID",
-#    "loop_delay" : 500,
-#    "csv_prune_max" : 5,
-#    "json_prune_max" : 10
-# }
-# parser = ConfigParser(default_confs)
-# TODO:  delete the above default dict after testing fallback.
-
-parser = ConfigParser()
-config_file_name = 'erras.ini'
-section_name = "erras"
-# This constructs a file path to the config_file_name in the same directory as the script file.
-config_path = str(pathlib.Path(__file__).with_name(config_file_name))
-with open(config_path) as config_file:
-    parser.read_file(config_file)
-
-wa_api_client_id = parser.get(section_name, "wa_api_client_id")
-wa_api_client_secret = parser.get(section_name, "wa_api_client_secret")
-credential_name = parser.get(section_name, "credential_name")
-credential_key = parser.get(section_name, "credential_key")
-api_key = parser.get(section_name, "api_key")
-filter_query = parser.get(section_name, "filter_query", fallback="status eq Active or status eq 'Pending - Renewal'")
-request_url_root = parser.get(section_name, "request_url_root")
-csv_backup_filename_root = parser.get(section_name, "csv_backup_filename_root", fallback="erras_backup_members_")
-csv_filename_temp = parser.get(section_name, "csv_filename_temp", fallback="erras_members_new.csv")
-csv_filename = parser.get(section_name, "csv_filename", fallback="erras_members.csv")
-apricot_response_root = parser.get(section_name, "apricot_response_root", fallback="wild_apricot_response_")
-loop_delay = parser.getint(section_name, "loop_delay", fallback=500)
-csv_prune_max = parser.getint(section_name, "csv_prune_max", fallback=5)
-json_prune_max = parser.getint(section_name, "json_prune_max", fallback=5)
-log_filename = parser.get(section_name, "members_log_filename", fallback="erras_members.log")
-keypad_field_names_string = parser.get(section_name, "keypad_field_names", fallback="Keypad")
-rfid_field_names_string = parser.get(section_name, "rfid_field_names", fallback="RFID")
-# Split up the key_fields_string into a list.
-# TODO: look into this later for split with escape
-# https://stackoverflow.com/questions/18092354/python-split-string-without-splitting-escaped-character
-keypad_field_names = keypad_field_names_string.split(",")
-rfid_field_names = rfid_field_names_string.split(",")
-
-# set up logger
-logger_name = "erras_members"
-logger_format = '%(asctime)s %(levelname)s %(message)s'
-log = logging.getLogger(logger_name)
-log.setLevel(logging.DEBUG)
-formatter = logging.Formatter(logger_format)
-
-# https://docs.python.org/2/library/logging.handlers.html
-# how often the log file is rotated is interval * when
-# when = S/M/H/D/W0-W6/midnight
-# so when='S', interval=500 means every 500 seconds.
-handler = TimedRotatingFileHandler(log_filename, when='D', interval=1, backupCount=20)
-handler.setFormatter(formatter)
-# handler.setLevel(logging.INFO)
-handler.setLevel(logging.DEBUG)
-
-log.addHandler(handler)
-# log.addHandler(JournalHandler())
-
-# Log the field names
-for field in keypad_field_names:
-    log.debug("keypad field names: %s" % field)
-for field in rfid_field_names:
-    log.debug("RFID field names: %s" % field)
-
-errasfile = ErrasFiles(keypad_field_names, rfid_field_names, log)
-
-api = WaApiClient(wa_api_client_id, wa_api_client_secret, log, debug=True)
-api.authenticate_with_contact_credentials(credential_name, credential_key)
-
-log.info("Starting request loop.")
-while(True):
-    log.info("########################### requesting member data ############################")
-    params = { '$filter': filter_query,
-               '$async': 'false' }
-
-    request_url = request_url_root + '?' + urllib.parse.urlencode(params)
-    log.debug("Request url is: %s" % request_url)
-
-    contacts = api.execute_request(request_url)
-    # each contact is an ApiObject instance
-    contact_list = contacts.Contacts
-    log.info("There are %d contacts in results." % len(contact_list))
+def main():
+    # TODO:  delete this default dict after testing fallback.
+    # default_confs = {
+    #    "filter_query" : "status eq Active or status eq 'Pending - Renewal'",
+    #    "csv_backup_filename_root" : "erras_backup_members_",
+    #    "csv_filename_temp" : "erras_members_new.csv",
+    #    "csv_filename" : "erras_members.csv",
+    #    "apricot_response_root" : "wild_apricot_response_",
+    #    "members_log_filename" : "erras_members.log",
+    #    "keypad_field_names" : "Keypad",
+    #    "rfid_field_names" : "RFID",
+    #    "loop_delay" : 500,
+    #    "csv_prune_max" : 5,
+    #    "json_prune_max" : 10
+    # }
+    # parser = ConfigParser(default_confs)
+    # TODO:  delete the above default dict after testing fallback.
     
-    # Save the newly downloaded member data in a backup file
-    csv_backup_filename = csv_backup_filename_root + api.get_timestamp() + ".csv"
-    errasfile.print_contacts_csv(contact_list, csv_backup_filename)
-    # And in a temp file.
-    errasfile.print_contacts_csv(contact_list, csv_filename_temp)
+    parser = ConfigParser()
+    config_file_name = 'erras.ini'
+    section_name = "erras"
+    # This constructs a file path to the config_file_name in the same directory as the script file.
+    config_path = str(pathlib.Path(__file__).with_name(config_file_name))
+    with open(config_path) as config_file:
+        parser.read_file(config_file)
+    
+    wa_api_client_id = parser.get(section_name, "wa_api_client_id")
+    wa_api_client_secret = parser.get(section_name, "wa_api_client_secret")
+    credential_name = parser.get(section_name, "credential_name")
+    credential_key = parser.get(section_name, "credential_key")
+    api_key = parser.get(section_name, "api_key")
+    filter_query = parser.get(section_name, "filter_query", fallback="status eq Active or status eq 'Pending - Renewal'")
+    request_url_root = parser.get(section_name, "request_url_root")
+    csv_backup_filename_root = parser.get(section_name, "csv_backup_filename_root", fallback="erras_backup_members_")
+    csv_filename_temp = parser.get(section_name, "csv_filename_temp", fallback="erras_members_new.csv")
+    csv_filename = parser.get(section_name, "csv_filename", fallback="erras_members.csv")
+    apricot_response_root = parser.get(section_name, "apricot_response_root", fallback="wild_apricot_response_")
+    loop_delay = parser.getint(section_name, "loop_delay", fallback=500)
+    csv_prune_max = parser.getint(section_name, "csv_prune_max", fallback=5)
+    json_prune_max = parser.getint(section_name, "json_prune_max", fallback=5)
+    log_filename = parser.get(section_name, "members_log_filename", fallback="erras_members.log")
+    keypad_field_names_string = parser.get(section_name, "keypad_field_names", fallback="Keypad")
+    rfid_field_names_string = parser.get(section_name, "rfid_field_names", fallback="RFID")
+    # Split up the key_fields_string into a list.
+    # TODO: look into this later for split with escape
+    # https://stackoverflow.com/questions/18092354/python-split-string-without-splitting-escaped-character
+    keypad_field_names = keypad_field_names_string.split(",")
+    rfid_field_names = rfid_field_names_string.split(",")
+    
+    # set up logger
+    logger_name = "erras_members"
+    logger_format = '%(asctime)s %(levelname)s %(message)s'
+    log = logging.getLogger(logger_name)
+    log.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(logger_format)
+    
+    # https://docs.python.org/2/library/logging.handlers.html
+    # how often the log file is rotated is interval * when
+    # when = S/M/H/D/W0-W6/midnight
+    # so when='S', interval=500 means every 500 seconds.
+    handler = TimedRotatingFileHandler(log_filename, when='D', interval=1, backupCount=20)
+    handler.setFormatter(formatter)
+    # handler.setLevel(logging.INFO)
+    handler.setLevel(logging.DEBUG)
+    
+    log.addHandler(handler)
+    # log.addHandler(JournalHandler())
+    
+    # Log the field names
+    for field in keypad_field_names:
+        log.debug("keypad field names: %s" % field)
+    for field in rfid_field_names:
+        log.debug("RFID field names: %s" % field)
+    
+    errasfiles = ErrasFiles(keypad_field_names, rfid_field_names, log)
+    
+    api = WaApiClient(wa_api_client_id, wa_api_client_secret, log, debug=True)
+    api.authenticate_with_contact_credentials(credential_name, credential_key)
+    
+    log.info("Starting request loop.")
+    while(True):
+        log.info("########################### requesting member data ############################")
+        params = { '$filter': filter_query,
+                   '$async': 'false' }
+    
+        request_url = request_url_root + '?' + urllib.parse.urlencode(params)
+        log.debug("Request url is: %s" % request_url)
+    
+        contacts = api.execute_request(request_url)
+        # each contact is an ApiObject instance
+        contact_list = contacts.Contacts
+        log.info("There are %d contacts in results." % len(contact_list))
+        
+        # Save the newly downloaded member data in a backup file
+        csv_backup_filename = csv_backup_filename_root + errasfiles.get_timestamp() + ".csv"
+        errasfiles.print_contacts_csv(contact_list, csv_backup_filename)
+        # And in a temp file.
+        errasfiles.print_contacts_csv(contact_list, csv_filename_temp)
+    
+        # Note, do not use across filesystem boundaries.  File rename is
+        # only atomic on unix if both new and old are on the same
+        # filesystem.
+        os.rename(csv_filename_temp, csv_filename)
+        log.info("Member data saved in filename %s" % csv_filename)
+        directory = os.getcwd()
+        errasfiles.prune(directory, csv_backup_filename_root, ".csv", csv_prune_max)
+        errasfiles.prune(directory, apricot_response_root, ".json", json_prune_max)
+    
+        log.info("Sleeping for %d seconds." % loop_delay)
+        time.sleep(loop_delay)
 
-    # Note, do not use across filesystem boundaries.  File rename is
-    # only atomic on unix if both new and old are on the same
-    # filesystem.
-    os.rename(csv_filename_temp, csv_filename)
-    log.info("Member data saved in filename %s" % csv_filename)
-    directory = os.getcwd()
-    errasfile.prune(directory, csv_backup_filename_root, ".csv", csv_prune_max)
-    errasfile.prune(directory, apricot_response_root, ".json", json_prune_max)
+main()
 
-    log.info("Sleeping for %d seconds." % loop_delay)
-    time.sleep(loop_delay)
